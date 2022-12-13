0476664e7d41354066facf9c3df6ad328fc1da71
Nurlan Memmedov <nurlan.memmedov8818@gmail.com>
2022-12-13 21:07:59 +0400

    Add project files.

---
+++ ConsoleApp9/Book.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    public class Book
+    {
+        public string Name { get; set; }
+        public string AutorName { get; set; }
+        public int PageCOunt { get; set; }
+        public double Price { get; set; }
+        private string BookCode { get; set; }
+        private static int Queue { get; set; }
+
+        public Book(string name, string authorName, int pageCount, double price)
+        {
+            Queue++;
+            Name = name;
+            AutorName = authorName;
+            PageCOunt = pageCount;
+            Price = price;
+            string FLater = Name[0].ToString();
+
+            string Number =Queue.ToString();
+            BookCode = FLater +"-"+Number;
+            Console.WriteLine(BookCode);
+        }
+    }
+}
---
+++ ConsoleApp9/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Melik Memmed","Kenan",31,69);
+            Book book2 = new Book("Memmed Melik","Isa",72,31.69);
+            Book book3 = new Book("Melik ve Memmed", "Nurlan", 69, 31.69);
+        }
+    }
+}
